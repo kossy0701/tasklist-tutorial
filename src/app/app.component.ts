b36c79bb4c257c76da0914d5f85f3184836069fe
kossy0701 <0421kossy0421@gmail.com>
2020-11-08 11:33:26 +0900

    create crud function with localstorage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,37 +11,17 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 })
 export class AppComponent implements OnInit {
   public selectedIndex = 0;
+  tasks: { name: string }[] = [
+    { name: 'タスク1' },
+    { name: 'タスク2' },
+  ];
+  task: string;
   public appPages = [
     {
       title: 'Inbox',
       url: '/folder/Inbox',
       icon: 'mail'
     },
-    {
-      title: 'Outbox',
-      url: '/folder/Outbox',
-      icon: 'paper-plane'
-    },
-    {
-      title: 'Favorites',
-      url: '/folder/Favorites',
-      icon: 'heart'
-    },
-    {
-      title: 'Archived',
-      url: '/folder/Archived',
-      icon: 'archive'
-    },
-    {
-      title: 'Trash',
-      url: '/folder/Trash',
-      icon: 'trash'
-    },
-    {
-      title: 'Spam',
-      url: '/folder/Spam',
-      icon: 'warning'
-    }
   ];
   public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
 
@@ -53,6 +33,14 @@ export class AppComponent implements OnInit {
     this.initializeApp();
   }
 
+  addTask() {
+    this.tasks.push({
+      name: this.task
+    });
+    localStorage.tasks = JSON.stringify(this.tasks);
+    this.task = '';
+  }
+
   initializeApp() {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
@@ -61,9 +49,8 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    const path = window.location.pathname.split('folder/')[1];
-    if (path !== undefined) {
-      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    if ('tasks' in localStorage) {
+      this.tasks = JSON.parse(localStorage.tasks);
     }
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import {FormsModule } from '@angular/forms';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
   imports: [
     BrowserModule,
     IonicModule.forRoot(),
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [
     StatusBar,
